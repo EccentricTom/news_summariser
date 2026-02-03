d967b2626e3703813c2ec63271814210748512b4
Tom <tom@inmail.me>
2026-02-03 12:29:46 +0100

    added explicit command to not create new table if one exists

--- src/news_summariser/db/init.py
+++ src/news_summariser/db/init.py
@@ -2,4 +2,4 @@ from sqlalchemy.engine import Engine
 from .schema import Base
 
 def create_tables(engine: Engine) -> None:
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine, checkfirst=True)
